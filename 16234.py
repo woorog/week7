239b98c737bb9728572c4a5010fc6725570bf3a4
woorog <nohanwoo@gmail.com>
2024-03-26 09:58:40 +0900

    3-26

--- 16234.py
+++ 16234.py
@@ -1,3 +1,4 @@
+import copy
 import sys
 from collections import deque
 
@@ -10,43 +11,61 @@ for i in range(n):
 
 
 
-print(board)
 
-sumlist=[0]*n*n
+sumlist=[0]*10000000
 now=0
 dis=[(0,1),(1,0),(0,-1),(-1,0)]
-
+cnt=0
 def move(i, k,now):
     temp=now
     q=deque()
     q.append((i,k))
+    count=1
     while q:
         y,x=q.popleft()
         for dy,dx in dis:
             dy+=y
             dx+=x
-            if 0<=dy<n and 0<=dx<n and visited[dy][dx]==0 and  small<=abs(board[y][x] - board[dy][dx])>=big :
+            if 0<=dy<n and 0<=dx<n and visited[dy][dx]==0 and  small<=abs(board[y][x] - board[dy][dx])<=big :
                 visited[dy][dx]=temp
-                sumlist[temp]+=visited[dy][dx]
+                sumlist[temp]+=board[dy][dx]
+                count+=1
                 q.append((dy,dx))
 
 
+    one=int(sumlist[temp]/count)
+    for i in range(n):
+        for k in range(n):
+            if visited[i][k]==now:
+                board[i][k]=one
+
+    print(board)
 
 
 
-visited=[[0]*n for _ in range(n)]
-# while sum(sumlist)==0:
 
+
+while True:
+    ans=0
+    visited=[[0]*n for _ in range(n)]
+    cboard=copy.deepcopy(board)
     for i in range(n):
         for k in range(n):
             for dy,dx in dis:
-                if 0<=i+dy<n and 0<=k+dx<n and visited[i+dy][k+dx]==0:
+                if 0<=i+dy<n and 0<=k+dx<n and visited[i+dy][k+dx]==0  and  small<=abs(board[i][k] - board[i+dy][k+dx])<=big :
                     now+=1
                     visited[i][k]=now
                     sumlist[now]+=board[i][k]
                     move(i,k,now)
+                    ans=1
                 else:
                     visited[i][k]=-1
+    if ans==0:
+        break
+    cnt+=1
 
 
-    print(sumlist)
+
+# print(sumlist)
+# print(now)
+print(cnt)
--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-C, N = map(int, input().split())  # C: 목표 고객 수, N: 도시의 개수
-dp = [float('inf')] * (C+100)  # 고객 수 C를 달성하기 위한 최대 비용을 고려하여 초기화
-dp[0] = 0  # 0명의 고객을 달성하는데 필요한 비용은 0
+import sys
 
-for _ in range(N):
-    cost, customers = map(int, input().split())
-    for i in range(customers, C+100):
-        dp[i] = min(dp[i], dp[i-customers] + cost)
+ipt = sys.stdin.readline().split()
 
-# 목표 고객 수 C를 달성할 수 있는 최소 비용을 찾습니다.
-for i in range(C, C+100):
-    if dp[i] < float('inf'):
-        print(dp[i])
-        break
+print(ipt + "??!", end="")
